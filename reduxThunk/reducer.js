958ea58c460abe1d16a1bfe57bd8c4c35bc5004d
tttm1501 <nam0968024068@gmail.com>
2022-04-20 20:39:59 +0700

    Add ex Redux Thunk

---
+++ reduxThunk/reducer.js
@@ -0,0 +1,20 @@
+import { FETCH_USER_SUCCESS, LOGIN_SUCCESS } from "./actions";
+ 
+// khởi tạo giá trị mặc định cho state gốc.
+ 
+const initialState = {
+    users: [],
+    userlogined: {}
+};
+// Khởi tạo reducer
+const rootReducer = (state = initialState, action) => {
+// Handle các actions gửi lên
+    switch (action.type) {
+        case LOGIN_SUCCESS:
+            return { ...state, userlogined: action.payload }
+        case FETCH_USER_SUCCESS:
+            return { ...state, users: action.payload };
+    }
+    return state;
+}
+export default rootReducer;
